634689f906409c4621b8e134152b5445d4a74f89
Vivien <viviendebuchy@yahoo.fr>
2016-04-12 19:34:27 +0200

    Ajout fonction diagonale

--- test.py
+++ test.py
@@ -1,11 +1,21 @@
+import math
+
 """FORMULE DE GEOMETRIE DE BASE SUR DES RECTANGLE"""
+
 def air(a, b):
     return a*b
+
 def perimetre(a, b):
     return a+b
 
-rectangle = (5, 6)
+def diagonale(a, b):
+    diagonale = math.sqrt(a*a + b*b)
+    return diagonale
+    
+
+rectangle = (1, 1)
 a, b = rectangle
 print(air(a, b))
+print(diagonale(a, b))
 
 
