9199186c5633291629ddcf44d5e85236e786d0d6
Vivien <viviendebuchy@yahoo.fr>
2016-04-12 19:26:42 +0200

    Formule de geometrie

---
+++ test.py
@@ -0,0 +1,11 @@
+"""FORMULE DE GEOMETRIE DE BASE SUR DES RECTANGLE"""
+def air(a, b):
+    return a*b
+def perimetre(a, b):
+    return a+b
+
+rectangle = (5, 6)
+a, b = rectangle
+print(air(a, b))
+
+
