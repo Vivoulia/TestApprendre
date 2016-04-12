3e76ade6f6f43275f5b229ed509b1bae1491c2f2
Pascakk <pascal.beroule@gmail.com>
2016-04-12 21:20:34 +0200

    et bim

--- test.py
+++ test.py
@@ -12,12 +12,26 @@ def RectDiagonale(a, b):
     diagonale = math.sqrt(a*a + b*b)
     return diagonale
 
+"""FORMULE DE GEOMETRIE DE BASE SUR DES CARRES"""
 
-"""SI TU VOIS CE COM C'EST QUE SA MARCHE"""
+def CarrAir(a):
+    return a*a
+
+def CarrPerimetre(a):
+    return a*4
+
+def CarrDiagonale(a):
+    diagonale = math.sqrt(a*a)
+    return diagonale
+
 
 rectangle = (1, 1)
 a, b = rectangle
 print(RectAir(a, b))
 print(RectDiagonale(a, b))
 
+carre = 1
+a = carre
+print(CarrAir(a))
+print(CarrDiagonale(a))
 
