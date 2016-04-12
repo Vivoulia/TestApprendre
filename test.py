521d330335d358faadaeaad2b0f03e3e9024596b
Vivien <viviendebuchy@yahoo.fr>
2016-04-12 21:17:54 +0200

    Mise a jour des fonctions

--- test.py
+++ test.py
@@ -6,17 +6,19 @@ def RectAir(a, b):
     return a*b
 
 def RectPerimetre(a, b):
-    return a+b
+    return (a+b)*2
 
 def RectDiagonale(a, b):
     diagonale = math.sqrt(a*a + b*b)
     return diagonale
 
+def RectSePresenter(a, b):
+    print("je suis un rectangle de longueur: ", a, "et de largeur: ", b)
 
-"""SI TU VOIS CE COM C'EST QUE SA MARCHE"""
 
 rectangle = (1, 1)
 a, b = rectangle
+RectSePresenter(a, b)
 print(RectAir(a, b))
 print(RectDiagonale(a, b))
 
